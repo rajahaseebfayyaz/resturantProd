829789496dc63aa325411fbe67de8947d2fec42c
rajahaseebfayyaz <rajahaseebfayyaz@gmail.com>
2023-06-25 19:41:02 +0530

    changes the project name

--- resturantly/urls.py
+++ resturantly/urls.py
@@ -1,4 +1,4 @@
-"""restaurantWebApp URL Configuration
+"""resturantly URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
